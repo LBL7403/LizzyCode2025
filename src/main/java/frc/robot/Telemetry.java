8a8d9be842f9e26d9296b319e769111b6e43d110
Nico <henaonicolas0222@gmail.com>
2025-01-18 12:28:12 -0500

    Added the constants class and set the speed to a percent

--- src/main/java/frc/robot/Telemetry.java
+++ src/main/java/frc/robot/Telemetry.java
@@ -19,6 +19,7 @@ import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
+import frc.robot.Constants.Swerve;
 
 public class Telemetry {
     private final double MaxSpeed;
@@ -28,8 +29,8 @@ public class Telemetry {
      * 
      * @param maxSpeed Maximum speed in meters per second
      */
-    public Telemetry(double maxSpeed) {
-        MaxSpeed = maxSpeed;
+    public Telemetry() {
+        MaxSpeed = Swerve.speedP;
         SignalLogger.start();
     }
 
@@ -76,6 +77,10 @@ public class Telemetry {
         m_moduleMechanisms[3].getRoot("RootDirection", 0.5, 0.5)
             .append(new MechanismLigament2d("Direction", 0.1, 0, 0, new Color8Bit(Color.kWhite))),
     };
+
+    public MechanismLigament2d[] getDirections(){
+        return m_moduleDirections;
+    }
 
     private final double[] m_poseArray = new double[3];
     private final double[] m_moduleStatesArray = new double[8];
